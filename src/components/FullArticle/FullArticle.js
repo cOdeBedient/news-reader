f29e654138eaa9aec837ee65929746ec8b3f842c
Adam Bedient <adambedient@gmail.com>
2024-04-30 14:47:13 -0400

    Fetch data from api to ensure it works

--- src/components/FullArticle/FullArticle.js
+++ src/components/FullArticle/FullArticle.js
@@ -4,7 +4,6 @@ import {useParams} from 'react-router-dom'
 
 function FullArticle({ articles }) {
     const { id } = useParams()
-    console.log('id', id)
     const currentArticle = articles.find(article => article.id.toString() === id)
 
 console.log('currentArticle', currentArticle)
